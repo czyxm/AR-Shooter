7a96ef63f6eb0fe6e132e10c7b7d7b905fe12ad2
yplusone <yangjiani233@outlook.com>
2020-05-27 22:11:04 +0800

    yjn提交

--- Assets/Scripts/MainGame/Player.cs
+++ Assets/Scripts/MainGame/Player.cs
@@ -7,22 +7,27 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public int health = 100;
-    private Text healthText;
-
+    public int healthMax = 100;
+    public Text healthText;
+    public Image healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        healthText = GameObject.Find("Health").GetComponent<Text>();
-        healthText.text = "Health: " + health;
+        //healthText = GameObject.Find("Health").GetComponent<Text>();
+        //healthBar = GetComponent<Image>();
+        //healthText.text = "Health: " + health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Health: " + health;
+        healthText.text = health.ToString() + "/" + healthMax.ToString();
+        //health = health - 1;
+        Debug.Log(health);
+        healthBar.fillAmount = (float)health / (float)healthMax;
         if (health <= 0)
         {
-            SceneManager.LoadScene("Failed");
+            SceneManager.LoadScene("Fail");
         }
     }
 }
---
+++ Assets/Scripts/ManiMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ManiMenu : MonoBehaviour
+{
+    public Slider BGMSlider;
+    public AudioSource BGMSource;
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void ChangeVolume()
+    {
+        Debug.Log("ChangeVolume");
+        Debug.Log(BGMSlider.value);
+        BGMSource.volume = BGMSlider.value;
+    }
+}
